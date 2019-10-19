2b54042f7d2075e6b6d85200c4f5af20078a4d93
Hugo Herrera <hugoherrera2022@u.northwestern.edu>
2019-10-19 13:48:11 -0500

    Updates

---
+++ precache-manifest.75e9487f0c277fdcecf39a63f1c0a3cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6dc7cf533d5a5924c63b770cccb3a8",
+    "url": "/spotify-music-ranker/index.html"
+  },
+  {
+    "revision": "1b53acd876cf5c8f294b",
+    "url": "/spotify-music-ranker/static/css/main.14fdcc91.chunk.css"
+  },
+  {
+    "revision": "901e37d9ce6062fb70f7",
+    "url": "/spotify-music-ranker/static/js/2.8bee7e79.chunk.js"
+  },
+  {
+    "revision": "1b53acd876cf5c8f294b",
+    "url": "/spotify-music-ranker/static/js/main.cf5d7912.chunk.js"
+  },
+  {
+    "revision": "084f38d29ad61b395318",
+    "url": "/spotify-music-ranker/static/js/runtime~main.14a0d036.js"
+  },
+  {
+    "revision": "36ffb61c6f79b00abd362a470cbcac43",
+    "url": "/spotify-music-ranker/static/media/alfonso-scarpa-e12EkMYv44U-unsplash.36ffb61c.jpg"
+  }
+]);
