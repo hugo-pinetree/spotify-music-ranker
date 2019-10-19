33ea1730af7709f05bff1e573a753788aed3117f
Hugo Herrera <hugoherrera2022@u.northwestern.edu>
2019-10-19 01:54:26 -0500

    url name change attempt 4

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const client_id = '8aabf0573c944d8e997334df6c782355';
-export const redirect_uri = 'http://discoveryourmood.com/';
+export const redirect_uri = 'https://discoveryourmood';
 export const auth_endpoint = 'https://accounts.spotify.com/authorize';
 export var scopes = 'user-top-read';
 
