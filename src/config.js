d299473d149ffa576fb7ea013280000d2430acd3
Hugo Herrera <hugoherrera2022@u.northwestern.edu>
2019-11-15 21:03:52 -0600

    Redirect address update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const client_id = '8aabf0573c944d8e997334df6c782355';
-export const redirect_uri = 'https://hugo-pinetree.github.io/spotify-music-ranker/callback';
+export const redirect_uri = 'https://hugo-pinetree.github.io/spotify-music-ranker';
 export const auth_endpoint = 'https://accounts.spotify.com/authorize';
 export var scopes = 'user-top-read';
 
