b21c71940a71f6cbbb14f347ef2fc2df31d4b1c6
Hugo Herrera <hugoherrera2022@u.northwestern.edu>
2020-01-11 14:57:19 -0600

    Updates

---
+++ precache-manifest.db7e34d8f10384ad7bbc817389cae9d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b76c83a9d860de1b90d2334f3efe47b",
+    "url": "/spotify-music-ranker/index.html"
+  },
+  {
+    "revision": "2128e88520ab2af454b4",
+    "url": "/spotify-music-ranker/static/css/main.e5828a95.chunk.css"
+  },
+  {
+    "revision": "041408e2aaebbcac135b",
+    "url": "/spotify-music-ranker/static/js/2.3b7a635f.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/spotify-music-ranker/static/js/2.3b7a635f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2128e88520ab2af454b4",
+    "url": "/spotify-music-ranker/static/js/main.07d204b0.chunk.js"
+  },
+  {
+    "revision": "0f2d4770961f32bd23e0",
+    "url": "/spotify-music-ranker/static/js/runtime-main.9307cb6c.js"
+  },
+  {
+    "revision": "36ffb61c6f79b00abd362a470cbcac43",
+    "url": "/spotify-music-ranker/static/media/alfonso-scarpa-e12EkMYv44U-unsplash.36ffb61c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-music-ranker/precache-manifest.43306a447a67dd136a697f3af2ba2791.js"
+  "/spotify-music-ranker/precache-manifest.db7e34d8f10384ad7bbc817389cae9d6.js"
 );
 
 self.addEventListener('message', (event) => {
